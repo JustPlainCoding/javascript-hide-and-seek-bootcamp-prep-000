f54aea2c618bd97ed78963f52b4a62fd5d12a812
Eric J Wilhite <ericjwilhite@gmail.com>
2019-05-23 22:14:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+function getFirstSelector(selector) {
+  var selected = document.querySelector(selector);
+  return selected;
+}
 
+function nestedTarget() {
+  var targetted = document.querySelector("#nested .target");
+  return targetted;
+}
+
+function increaseRankBy(n) {
+  const lis = document
+  .getElementById('app')
+  .querySelectorAll('ul.ranked-list li');
+  for(let i = 0;i<lis.length;i++){
+    let inted = parseInt(lis[i].innerHTML);
+    lis[i].innerHTML = (inted + n).toString();
+  }
+}
+
+function deepestChild() {
+  let divs = document
+  .getElementById("grand-node")
+  .querySelectorAll("div");
+  debugger;
+  let next = [];
+  let length = 0;
+  while(length !== 1) {
+  length = next.length;
+	for(let i =0;i < divs.length;i++) {
+		next.push(divs[i]);
+	  }
+	  divs = next.shift();
+  }
+  return divs
+}
